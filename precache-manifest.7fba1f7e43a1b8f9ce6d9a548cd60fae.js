73ffb111c2972a01f2842db96776bc3ee5b3ded8
Daniel Bieniek <daniel.bieniek97@gmail.com>
2021-10-12 02:00:46 +0200

    Updates

---
+++ precache-manifest.7fba1f7e43a1b8f9ce6d9a548cd60fae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f76dc81fcdfb6a201eade9770f8ef52",
+    "url": "/Puzzle15/index.html"
+  },
+  {
+    "revision": "7c2651d43543955672c3",
+    "url": "/Puzzle15/static/css/main.37a133e9.chunk.css"
+  },
+  {
+    "revision": "73d7d2a4fb7760bb5792",
+    "url": "/Puzzle15/static/js/2.8d89fcb1.chunk.js"
+  },
+  {
+    "revision": "7c2651d43543955672c3",
+    "url": "/Puzzle15/static/js/main.f3798778.chunk.js"
+  },
+  {
+    "revision": "1845a9cee79be68ebf54",
+    "url": "/Puzzle15/static/js/runtime~main.5c86082e.js"
+  }
+]);
